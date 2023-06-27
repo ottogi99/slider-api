8c76b4009af664d8cee36284b5e7576796ee963f
ottogi99 <ottogi99@gmail.com>
2023-06-27 23:30:54 +0900

    2023-06-27 first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ormConfig } from './orm.config';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot(ormConfig()),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/domain/user.entity.ts
@@ -0,0 +1,41 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { UserAuthority } from "./user-authority.entity";
+
+@Entity('user', { schema: 'looknfee' })
+export class User {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column('varchar', { name: 'kakao_id', length: 45})
+  kakaoId: string;
+
+  @Column('varchar', { name: 'email', length: 1000})
+  email: string;
+
+  @Column('varchar', { name: 'name', nullable: true, length: 45 })
+  name: string | null;
+
+  @Column('varchar', { name: 'gender', length: 10 })
+  gender: string;
+
+  @Column('varchar', { name: 'phone', length: 20 })
+  phone: string;
+
+  @Column('varchar', { name: 'birth', length: 10 })
+  birth: string;
+
+  @Column('varchar', { name: 'profile_image', nullable: true, length: 200 })
+  profileImage: string | null;
+
+  @Column('timestamp', {
+    name: 'created_at',
+    nullable: true,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date | null;
+
+  @OneToMany(() => UserAuthority, (userAutority) => userAutority.user, {
+    eager: true,
+  })
+  authorities?: any[];
+}
